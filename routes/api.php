1a2aa65cc8a81388c65162a13403a7be1ec892d3
dmeckers <morph232@inbox.lv>
2025-06-12 09:44:14 +0300

    Fix route parameter validation for song streaming endpoint

--- routes/api.php
+++ routes/api.php
@@ -88,7 +88,7 @@ Route::prefix('v1')->middleware(['auth:sanctum'])->group(function () {
             Route::delete('/', [SongController::class, 'delete']);
         });
 
-        Route::prefix('/{code}')->where(['id' => RouteValidationEnum::HASH->value])->group(function () {
+        Route::prefix('/{code}')->where(['code' => RouteValidationEnum::HASH->value])->group(function () {
             Route::get('/stream', [SongController::class, 'streamedSong']);
         });
     });
